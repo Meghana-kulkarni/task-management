93dacbd768ebaf83e6a14d16cbe4362a42f9cb10
meghana <meghanakulkarni2002@gmail.com>
2026-05-10 02:57:39 +0530

    fixed task routes case issue

--- routes/taskroutes.js
+++ routes/taskroutes.js
@@ -6,7 +6,7 @@ const {
   getTasks,
   updateTask,
   deleteTask
-} = require("../controllers/TaskController");
+} = require("../controllers/taskController");
 
 const { protect } = require("../middleware/authMiddleware");
 
